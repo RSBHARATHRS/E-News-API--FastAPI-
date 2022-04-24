dd89f0918e5a049040dc44da44656c1f0b9bba88
Bharath R <78311020+RSBHARATHRS@users.noreply.github.com>
2022-04-24 10:33:50 +0530

    heroko required file added

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+from ast import excepthandler, keyword
+from hashlib import new
 import imp
+from multiprocessing.dummy import Array
 from typing import Optional
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import json
+
+import data
 
 app = FastAPI()
+origins = [
+    "http://localhost:8100",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Item(BaseModel):
@@ -15,14 +32,17 @@ class Item(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Uvicorn Server is runing"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/E-News/news-feeds")
+def getNewsFeeds():
+    return {"newsFeeds": data.news}
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/E-News/news-feeds/{news_id}")
+def update_item(news_id: int):
+    for news in data.news:
+        if news["newsId"] == news_id:
+            return {"news": news, "message": "Success"}
+    return {"message": "result Not Found"}
